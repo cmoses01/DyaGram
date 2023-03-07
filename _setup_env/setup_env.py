917f83c52a3648c13ca21280ea039ac724fec19a
cmoses01 <chrismoses121@gmail.com>
2023-03-07 17:44:28 -0600

    Beginning of inventory file

---
+++ _setup_env/setup_env.py
@@ -0,0 +1,32 @@
+#enable lldp, cdp, RESTCONF
+from netmiko import ConnectHandler
+
+netmiko_args = {
+    "device_type": "cisco_ios",
+    "host": "",
+    "username": "cisco",
+    "password": "cisco"
+}
+
+routers = ['10.10.20.175']
+
+nxos_switches = ['10.10.20.177', "10.10.20.178"]
+
+for r in routers:
+    netmiko_args['host'] = r
+    dev = ConnectHandler(**netmiko_args)
+    dev.send_config_set("lldp run")
+    dev.send_config_set("cdp run")
+    dev.send_config_set("restconf")
+    dev.send_config_set(['interface range gi4-5', "cdp enable"])
+    dev.send_config_set(['interface range gi4-5', "lldp enable"])
+
+    dev.disconnect()
+
+for s in nxos_switches:
+    netmiko_args['host'] = s
+    dev = ConnectHandler(**netmiko_args)
+    dev.send_config_set("feature lldp")
+    dev.send_config_set("feature restconf")
+    dev.send_config_set("feature nxapi")
+

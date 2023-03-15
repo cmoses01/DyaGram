7ab7b2308fdd8aa57b795da645cca1f1c24b8b85
cmoses01 <chrismoses121@gmail.com>
2023-03-15 17:26:54 -0500

    added packaging

--- app/discovery_worker.py
+++ app/discovery_worker.py
@@ -1,8 +1,8 @@
-from modules.dyagram import dyagram
+from dyagram.modules.dyagram import dyagram
 
 inventory = r"C:\Users\chrimos\PycharmProjects\DyaGram\app\inventory.yml"
 
 # main code
 
-dyagram = dyagram(inventory, initial=True)
+dyagram = dyagram(inventory)
 dyagram.discover()

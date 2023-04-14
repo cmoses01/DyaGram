5265e7fb110cdf18548a0a2f9c403275cd6b5214
cmoses01 <chrismoses121@gmail.com>
2023-04-14 14:39:43 -0500

    added test

--- tests/test_dyagram.py
+++ tests/test_dyagram.py
@@ -1,5 +1,7 @@
 import subprocess
 from datetime import datetime
+import os
+from netmiko import ConnectHandler
 
 
 def check_for_no_changes_in_state():
@@ -12,18 +14,26 @@ def check_for_no_changes_in_state():
     """
 
     test_number = 1
-    while True:
+    while test_number < 101:
 
         result = subprocess.run(['dyagram', 'discover','-v'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        if not "No changes in state" in result:
+        if not "No changes in state" in result and not 'Unable to connect' in result:
             file = open("test_results.txt", 'a')
             file.write(f"{str(datetime.now())} - !!!!!!FAILED TEST #{test_number}!!!!!!!\n")
             file.close()
             file = open("issues.txt", 'a')
             file.write(f"{str(datetime.now())} - FAILED TEST #{test_number}\n")
             file.close()
+            file = open("output.txt", 'a')
+            file.write(f"{str(datetime.now())}\n------\n{result}\n\n")
+            file.close()
             test_number += 1
 
+        elif 'Unable to connect' in result:
+            file = open("test_results.txt", 'a')
+            file.write(f"{str(datetime.now())} - PASSED TEST #{test_number}\n (UNABLE TO CONNECT TO DEVICE)")
+            file.close()
+            test_number += 1
         else:
             file = open("test_results.txt", 'a')
             file.write(f"{str(datetime.now())} - PASSED TEST #{test_number}\n")
@@ -31,5 +41,60 @@ def check_for_no_changes_in_state():
             test_number += 1
 
 
+
+def check_for_eigrp_neighbor_down():
+
+    """
+    THIS FUNC TESTS DYAGRAM REPEATEDLY ON A NETWORK THAT ISN'T CHANGING TO MAKE SURE EVERY TIME THE OUTPUT IS 'NO CHANGES IN STATE'
+
+    State file must already exist
+    :return:
+    """
+
+    test_number = 1
+    while test_number < 101:
+
+        result = subprocess.run(['dyagram', 'discover','-v'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        if "No changes in state" in result and not 'Unable to connect' in result:
+            file = open("test_results.txt", 'a')
+            file.write(f"{str(datetime.now())} - !!!!!!FAILED TEST #{test_number}!!!!!!!\n")
+            file.close()
+            file = open("issues.txt", 'a')
+            file.write(f"{str(datetime.now())} - FAILED TEST #{test_number}\n")
+            file.close()
+            file = open("output.txt", 'a')
+            file.write(f"{str(datetime.now())}\n------\n{result}\n\n")
+            file.close()
+            test_number += 1
+
+        elif 'Unable to connect' in result:
+            file = open("test_results.txt", 'a')
+            file.write(f"{str(datetime.now())} - PASSED TEST #{test_number}\n (UNABLE TO CONNECT TO DEVICE)")
+            file.close()
+            test_number += 1
+        elif 'eigrp' in result.lower():
+            file = open("test_results.txt", 'a')
+            file.write(f"{str(datetime.now())} - PASSED TEST #{test_number}\n")
+            file.close()
+            test_number += 1
+        else:
+
+            file = open("test_results.txt", 'a')
+            file.write(f"{str(datetime.now())} - !!!!!!FAILED TEST #{test_number}!!!!!!!\n")
+            file.close()
+            file = open("issues.txt", 'a')
+            file.write(f"{str(datetime.now())} - FAILED TEST #{test_number}\n")
+            file.close()
+            file = open("output.txt", 'a')
+            file.write(f"{str(datetime.now())}\n------\n{result}\n\n")
+            file.close()
+            test_number += 1
+
+
+
+
+
 if __name__ == "__main__":
+
+    #check_for_eigrp_neighbor_down()
     check_for_no_changes_in_state()
